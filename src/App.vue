2cc4eed0bd43d156301d2524af023fb2baeb00bc
CaptKrakenatic <kimsong432@gmail.com>
2022-09-22 14:14:49 +0700

    pagination feature

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
 <script setup lang="ts">
-import { computed, onMounted, ref } from "vue";
+import { computed, onMounted, reactive, ref } from "vue";
 import { useCountryStore } from "./state/countries";
-import { sortFunction } from "./utils/utils";
+import {
+  sortFunction,
+  orderCountriesByName,
+  paginateCountries,
+} from "./utils/utils";
 import { SortOption } from "./types";
 import CountryTable from "./components/CountryTable.vue";
 
@@ -17,6 +21,62 @@ onMounted(async () => {
   // fetch all the countries
   await useCountryStore().getAllCountries();
 });
+
+const sortedCountries = computed(() => {
+  return orderCountriesByName(countries.value, sortOption.value);
+});
+
+// pagination
+const pageInfo = reactive({
+  pageNumber: 1,
+  pageSize: 25,
+});
+
+const paginatedCountries = computed(() => {
+  return paginateCountries(
+    sortedCountries.value,
+    pageInfo.pageNumber,
+    pageInfo.pageSize
+  );
+});
+
+const shownRange = computed(() => {
+  const from =
+    pageInfo.pageNumber === 1
+      ? 1
+      : (pageInfo.pageNumber - 1) * pageInfo.pageSize;
+  const to = pageInfo.pageNumber * pageInfo.pageSize;
+
+  return { from, to };
+});
+
+const totalPageCount = computed(() =>
+  sortedCountries.value.length > 0
+    ? // round up
+      Math.ceil(sortedCountries.value.length / 25)
+    : 0
+);
+
+const goToPage = (pageNumber: number) => {
+  if (
+    pageNumber !== pageInfo.pageNumber ||
+    pageNumber > totalPageCount.value ||
+    pageNumber < 1
+  ) {
+    pageInfo.pageNumber = pageNumber;
+  }
+};
+
+const nextPage = () => {
+  if (pageInfo.pageNumber < totalPageCount.value) {
+    pageInfo.pageNumber++;
+  }
+};
+const previousPage = () => {
+  if (pageInfo.pageNumber - 1 > 0) {
+    pageInfo.pageNumber--;
+  }
+};
 </script>
 
 <template>
@@ -29,6 +89,42 @@ onMounted(async () => {
       <option value="desc">z-a</option>
     </select>
     <p v-if="countries.length < 1">loading data...</p>
-    <CountryTable :countries="countries" />
+    <CountryTable :countries="paginatedCountries" />
   </main>
+  <footer class="py-4">
+    <div class="w-full flex justify-center py-2">
+      <p>
+        Showing {{ shownRange.from }} - {{ shownRange.to }} of
+        {{ countries.length }}
+      </p>
+    </div>
+    <div
+      class="flex gap-2 w-full justify-center flex-wrap"
+      v-if="totalPageCount > 1"
+    >
+      <button
+        @click="previousPage"
+        aria-label="previous"
+        class="bg-green-200 px-3 py-2"
+      >
+        <img src="./assets/back.png" class="w-5" />
+      </button>
+      <button
+        v-for="i in totalPageCount"
+        :key="i"
+        @click="goToPage(i)"
+        class="bg-green-200 px-4 py-2"
+        :class="i === pageInfo.pageNumber ? 'bg-green-400' : ''"
+      >
+        <span>{{ i }}</span>
+      </button>
+      <button
+        @click="nextPage"
+        aria-label="next"
+        class="bg-green-200 px-3 py-2"
+      >
+        <img src="./assets/next.png" class="w-5" />
+      </button>
+    </div>
+  </footer>
 </template>
--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,4 +1,4 @@
-import { SortOption } from "../types";
+import { ICountry, SortOption } from "../types";
 
 /**
  * a generic sort function with ascending/descending option.
@@ -18,4 +18,31 @@ export const sortFunction = (
     return order === "asc" ? 1 : -1;
   }
   return 0;
+};
+
+export const orderCountriesByName = (
+  array: ICountry[],
+  order: "asc" | "desc" = "asc"
+) => {
+  return array.sort((a, b) => {
+    const countryA = a.name.official.toLowerCase();
+    const countryB = b.name.official.toLowerCase();
+    return sortFunction(countryA, countryB, order);
+  });
+};
+
+/**
+ * pagainate an array
+ * @param {Array} array the array itself
+ * @param {Number} pageNumber human readable, starts at 1 (default: 1)
+ * @param {Number} pageSize the number of items for each page (default: 25)
+ * @returns a new array of results
+ */
+export const paginateCountries = (
+  array: ICountry[],
+  pageNumber: number = 1,
+  pageSize: number = 25
+) => {
+  const lastIndex = pageNumber * pageSize;
+  return array.slice((pageNumber - 1) * pageSize, lastIndex);
 };
